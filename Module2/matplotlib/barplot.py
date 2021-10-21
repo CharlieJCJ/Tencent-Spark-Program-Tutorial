ac69c318d95d5b8c6c9414e536c33aa4b0daf73f
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2021-10-21 16:40:28 -0700

    update

--- Module2/matplotlib/barplot.py
+++ Module2/matplotlib/barplot.py
@@ -2,11 +2,11 @@ from matplotlib import pyplot as plt
 
 
 # 柱形图
-# plt.bar(类型, 频率) 
+# `plt.bar()`
+# 使用方法：plt.bar(类型, 频率)
 langs = ['C', 'C++', 'Java', 'Python', 'PHP']
 students = [23,17,35,29,12]
 plt.bar(langs, students)
-
 
 
 #*************************************#
@@ -14,6 +14,5 @@ plt.bar(langs, students)
 plt.xlabel('Programming language')
 plt.ylabel('Students')
 plt.title('Students using different programming languages')
-
 
 plt.show()
