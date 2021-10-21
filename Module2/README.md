ed1db840a4cb9942f001e0d758675ef39425eaeb
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2021-10-21 16:40:28 -0700

    update

---
+++ Module2/README.md
@@ -0,0 +1,20 @@
+# Module 2
+## 1. read_files
+## 2. Python Library `os`
+## 3. Python Library `matplotlib.pyplot`
+```python
+from matplotlib import pyplot as plt
+```
+1. `basic_plots.py`
+   - 一个最基础的line plot例子
+2. `plot_with_titles.py`
+   - plot里自定义参数
+3. `multiple_plots.py`
+   - 一张图表里可以graph多个line plots
+4. `barplot.py`
+   - plt 柱状图
+5. `histogram.py`
+   - 使用 `pandas` 来读取csv文件
+   - 选取其中的 `Age` 列，进行直方图的绘制
+6. `im_show.py`
+   - 使用 `opencv` 读取
--- Module2/matplotlib/subplot.py
+++ Module2/matplotlib/subplot.py
@@ -9,11 +9,11 @@ y2 = [45372, 48876, 53850, 57287, 63016,
             65998, 70003, 70000, 71496, 75370, 83640]
 
 
-# 你可以用子图来将图样（plot）放在均匀的坐标网格中。用 subplot 函数的时候，
+# 你可以用子图来将图样（plot）放在均匀的坐标网格中。用 `plt.subplot` 函数的时候，
 # 你需要指明网格的行列数量，以及你希望将图样放在哪一个网格区域中。
 # py.subplot(行序号，列序号，图序号)
 
-# 创建一个两行一列的子图集，在第一行绘制
+# 创建一个两行一列的子图集，在第一行绘制图表
 plt.subplot(2,1,1)
 plt.plot(x, y1, label = "All developer", color="blue", linewidth=2.5, linestyle="-", marker = ".")
 plt.ylabel('Median Salary (USD) 2019')
@@ -22,7 +22,7 @@ plt.xlim(24,36)
 plt.ylim(20000,90000)
 plt.legend(loc='upper left')
 
-# 创建一个两行一列的子图集，在第二行绘制
+# 创建一个两行一列的子图集，在第二行绘制图表
 plt.subplot(2,1,2)
 plt.plot(x, y2, label = "Python developer", color="red", linewidth=2.5, linestyle="--", marker = "p")
 plt.xlabel('Ages')
